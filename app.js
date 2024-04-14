dda19ae70f9a390c859bac64315532744f53c02d
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-14 14:55:49 +0000

    测试跳转

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-const switcher = window.onload;
-
-switcher.addEventListener(function() {
-    document.body.classList.toggle('light-theme');
-        document.body.classList.toggle('dark-theme');
-
-            console.log('[INFO]current class name: ' + className);
-    });
+function joinqq(){
+    location.assign("https://cn.bing.com")
+}
