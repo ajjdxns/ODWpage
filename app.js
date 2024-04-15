9faee4f76c7b582bcb369eb241e03a387131db42
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-15 22:17:29 +0800

    解决严重bug

--- app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu').addEventListener('toggle', function(event) {  
-        if (!this.close) {  
-            // 关闭动画  
-            this.style.animation = 'close 0.5s ease-in-out forwards';  
-        } else {  
-            // 清除动画，以便下次可以正常触发打开动画  
-            this.style.animation = '';  
-        }  
-});
