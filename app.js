61961bcf321d77115d09f25abf68e2fedcc68160
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-15 21:56:51 +0800

    JavaScript绑定

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-var join=document.getElementsByClassName("join")
-join.onclick=function(){
-    location.assign("https://cn.bing.com")
-}
+document.getElementById('menu').addEventListener('toggle', function(event) {  
+        if (!this.open) {  
+            // 关闭动画  
+            this.style.animation = 'collapse 0.5s ease-in-out forwards';  
+        } else {  
+            // 清除动画，以便下次可以正常触发打开动画  
+            this.style.animation = '';  
+        }  
+});
