52e8a460bb9d4868a28e22e78cae41e8e0bd5933
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-17 03:24:18 +0800

    在线统计

---
+++ app.js
@@ -0,0 +1,11 @@
+//获取在线人数
+$.ajax({
+url:"http://api.wer.plus/api/mcse" ,
+type:'get', 
+data:{
+host:'pm.rainplay.cn:12530',
+},
+success: function (res){
+let online = res.data.online
+console.log(online)
+document.getElementById('online').innerHTML="在线人数:"+online}})
