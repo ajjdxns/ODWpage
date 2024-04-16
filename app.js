7ed0de1dc7d7dc073d6e45265b6f8330d330543a
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-17 05:40:24 +0800

    更换获取源

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //获取在线人数
 $.ajax({
-    url:"http://api.wer.plus/api/mcse" ,
+    url:"https://api.imlazy.ink/mcapi" ,
     type:'get', 
     data:{
         host:'pm.rainplay.cn:12530',
 },
 success: function (res){
-    let online = res.data.online
+    let online = res.data.players_online
     console.log(online)
     document.getElementById('online').innerHTML="在线人数:"+online
     }
