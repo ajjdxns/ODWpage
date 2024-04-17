3d4e16ba716cfee20ebf471f749c83ed01aa38f0
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-17 23:40:58 +0800

    测试

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-//获取在线人数
-$.ajax({
-    url:"https://api.imlazy.ink/mcapi" ,
-    type:'get', 
-    data:{
-        host:'pm.rainplay.cn:12530',
-},
-success: function (res){
-    let online = res.data.players_online
-    console.log(online)
-    document.getElementById('online').innerHTML="在线人数:"+online
-    }
-});
+document.getElementById('online').innerHTML=navigator.platform
