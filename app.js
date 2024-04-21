beae5f854bff6335cc2c08569ea40fba17cc181d
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-21 13:22:58 +0800

    CNAME和自动跳转

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log(navigator.platform)
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+var isTablet = /iPad|Android/i.test(userAgent);
+var isDesktop = !isMobile && !isTablet;
+if (isDesktop){
+    console.log("[INFO]正在跳转至电脑版...")
+    window.location.assign("https://mcyouxi.asia/")
+}
