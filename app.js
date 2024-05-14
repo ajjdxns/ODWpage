636ffbdc38694ee6136663302707d3f577c3876a
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-05-14 20:01:45 +0800

    临时保存

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-var isTablet = /iPad|Android/i.test(userAgent);
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+var isTablet = /iPad|Android/i.test(navigator.userAgent);
 var isDesktop = !isMobile && !isTablet;
 if (isDesktop){
     console.log("[INFO]正在跳转至电脑版...")
